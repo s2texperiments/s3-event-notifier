7a39c8ca549e7a9f076a6badd745338eb715cb79
ste-xx <stefan.breitenstein@gmail.com>
2018-06-06 00:41:42 +0200

    struggeling with cf

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var response = require('cfn-response');
-exports.handler = function(event, context) {
-    var responseData = {Value: 12 * 5};
-    response.send(event, context, response.SUCCESS, responseData);
+var response = require('./cfn-response');
+exports.handler = async (event, context) => {
+    console.log(`REQUEST RECEIVED: \
+        ${JSON.stringify(event)}`);
+
+    response.send(
+        event,
+        context,
+        response.SUCCESS,
+        {
+            SubArn: 'some_arn'
+        });
+    return "fin"
 };
