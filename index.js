9cddf6f1448b79360cfc53ca1cedaf1a36318c8b
ste-xx <stefan.breitenstein@gmail.com>
2018-06-06 00:32:04 +0200

    struggeling with cf

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var response = require('cfn-response');
 exports.handler = function(event, context) {
     var input = parseInt(event.ResourceProperties.Input);
     var responseData = {Value: input * 5};
-    response.send(event, context, response.SUCCESS, responseData);
+    response.send(event, context, response.SUCCESS);
 };
