82bad0bfe66ca9a6038a034401a41acbda2088b0
ste-xx <stefan.breitenstein@gmail.com>
2018-06-05 23:49:04 +0200

    fix response issue

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
     response.send(
         event,
         context,
-        response.Success,
+        response.SUCCESS,
         {
             'SubArn': '<sub_arn>'
         });
