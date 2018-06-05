2bac4243624fc8b3e1c45294c77112101bab0432
ste-xx <stefan.breitenstein@gmail.com>
2018-06-06 01:49:02 +0200

    struggeling with cf

--- index.js
+++ index.js
@@ -7,26 +7,31 @@ exports.handler = async (event, context) => {
     console.log(`Context RECEIVED: \
         ${JSON.stringify(context)}`);
 
-    let parsedUrl = url.parse(event.ResponseURL);
+    let payload = JSON.stringify({
+        Status: "SUCCESS",
+        Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
+        PhysicalResourceId: context.logStreamName,
+        StackId: event.StackId,
+        RequestId: event.RequestId,
+        LogicalResourceId: event.LogicalResourceId,
+        NoEcho: false,
+        Data: {
+            SubArn: 'some_arn'
+        }
+    });
 
-    await fetch(`https://${parsedUrl.hostname}${parsedUrl.path}`, {
+    await fetch(`event.ResponseURL`, {
             method: 'PUT',
-            body: JSON.stringify({
-                Status: "SUCCESS",
-                Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
-                PhysicalResourceId: context.logStreamName,
-                StackId: event.StackId,
-                RequestId: event.RequestId,
-                LogicalResourceId: event.LogicalResourceId,
-                NoEcho: false,
-                Data: {
-                    SubArn: 'some_arn'
-                }
-            })
-        }).then(response => response.text())
+            headers: {
+                'content-type': '',
+                'content-length':payload.length
+            },
+            body: payload
+        }
+    ).then(response => response.text())
         .then(data => console.log(data))
         .catch(error => console.log("Err:" + error));
 
 
     return "fin"
-};
+}
