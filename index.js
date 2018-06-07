609dbd9152da212229cd5bcaffb1d8486e1e1a83
ste-xx <stefan.breitenstein@gmail.com>
2018-06-07 23:43:55 +0200

    add test cases

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const response = require("cf-fetch-response");
 
 exports.handler = async (event, context) => {
-    console.log(`REQUEST RECEIVED: \
-        ${JSON.stringify(event)}`);
-    console.log(`Context RECEIVED: \
-        ${JSON.stringify(context)}`);
+    console.log(`REQUEST RECEIVED: ${JSON.stringify(event)}`);
+    console.log(`Context RECEIVED: ${JSON.stringify(context)}`);
+    return doHandle(event, context)
+        .catch((e) => response.sendFail(event, context, e.message));
+};
 
-    return response.sendSuccess(event,context, {
-        data:{
-            SubArn: 'does_it_work_arn'
+async function doHandle(event, context) {
+
+    let {
+        ResourceProperties: {
+            S3Bucket: s3Bucket,
+            S3Prefix: s3Prefix,
+            EventLambdaArn: eventLambdaArn
+        }
+    } = event;
+    if(!s3Bucket || !s3Prefix || !eventLambdaArn){
+        throw `missing mandatory argument: s3Bucket=${s3Bucket} s3Prefix=${s3Prefix} eventLambdaArn=${eventLambdaArn}`
+    }
+    return response.sendSuccess(event, context, {
+        data: {
+            SubArn: 'wtf'
         }
     });
-};
+}
--- test/test.js
+++ test/test.js
@@ -1,23 +1,87 @@
 const expect = require('chai').expect;
-const spy = require('sinon').spy;
+const sinon = require('sinon');
 const fake = require('sinon').fake;
 
 const proxyquire = require('proxyquire').noCallThru();
 
 const fs = require('fs');
-const cfEvent = JSON.parse(fs.readFileSync('test/cfCreateEventData.json', 'utf8'));
+const cfCreateEvent = JSON.parse(fs.readFileSync('test/cfCreateEventData.json', 'utf8'));
+const cfUpdateEvent = JSON.parse(fs.readFileSync('test/cfUpdateEventData.json', 'utf8'));
+const cfDeleteEvent = JSON.parse(fs.readFileSync('test/cfDeleteEventData.json', 'utf8'));
+
 const cfContext = JSON.parse(fs.readFileSync('test/cfContextData.json', 'utf8'));
 
 
 describe('s3-event-notifier', () => {
 
+    let successFake;
+    let failFake;
+    let underTest;
+
+    beforeEach(() => {
+        successFake = fake.resolves("send suc");
+        failFake = fake.resolves("send fail");
+        underTest = proxyquire('../index.js', {
+            'cf-fetch-response': {
+                sendSuccess: successFake,
+                sendFail: failFake
+            }
+        });
+    });
+
+    afterEach(()=>{
+        sinon.restore();
+    });
+
+    it('missing S3Bucket argument should send fail response ', async () => {
+        //hard copy
+        let obj = JSON.parse(JSON.stringify(cfCreateEvent));
+        delete obj.ResourceProperties.S3Bucket;
+        await underTest.handler(obj, cfContext);
+        expect(successFake.callCount).to.equal(0);
+        expect(failFake.callCount).to.equal(1);
+    });
+
+    it('missing s3Prefix argument should send fail response ', async () => {
+        //hard copy
+        let obj = JSON.parse(JSON.stringify(cfCreateEvent));
+        delete obj.ResourceProperties.S3Prefix;
+        await underTest.handler(obj, cfContext);
+
+        expect(successFake.callCount).to.equal(0);
+        expect(failFake.callCount).to.equal(1);
+    });
+
+    it('missing EventLambdaArn argument should send fail response ', async () => {
+        //hard copy
+        let obj = JSON.parse(JSON.stringify(cfCreateEvent));
+        delete obj.ResourceProperties.EventLambdaArn;
+        await underTest.handler(obj, cfContext);
+
+        expect(successFake.callCount).to.equal(0);
+        expect(failFake.callCount).to.equal(1);
+    });
+
     it('create successful -> send success', async () => {
 
-        // let underTest = proxyquire('../index.js', {
-        //     'node-fetch': nodeFetchFake
-        // });
-        //
-        // await underTest.sendSuccess(cfEvent, cfContext);
+        console.log(cfCreateEvent.ResourceProperties.EventLambdaArn);
+        let expectedArn = "this:is:an:arn";
+
+        await underTest.handler(cfCreateEvent, cfContext);
+
+        expect(successFake.callCount).to.equal(1);
+        let [event, context, custom] = successFake.firstCall.args;
+        expect(event).to.deep.equal(cfCreateEvent);
+        expect(context).to.deep.equal(cfContext);
+        expect(custom).include.all.keys('data');
+
+        let data = custom.data;
+        expect(data).include({
+            'SubArn': expectedArn
+        });
+
+        expect(failFake.callCount).to.equal(0);
+
     });
 
     it('create failed (permission) -> send failed ', async () => {
