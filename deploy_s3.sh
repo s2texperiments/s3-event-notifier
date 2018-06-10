d4fab09498c6e331302a77e17f7484d4eec20515
ste-xx <stefan.breitenstein@gmail.com>
2018-06-10 23:03:21 +0200

    fix deploy script

--- deploy_s3.sh
+++ deploy_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir deploy
-zip -r deploy/s3-event-notifier.zip index.js s3Api.js handler.js node_modules/
+zip -r deploy/s3-event-notifier.zip index.js index_impl.js s3Api.js node_modules/
