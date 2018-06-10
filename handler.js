d656e3ccf974308b9d72e743cb14dc1b48bb8737
ste-xx <stefan.breitenstein@gmail.com>
2018-06-10 22:21:40 +0200

    ensure to send cf response

--- handler.js
+++ handler.js
@@ -4,14 +4,15 @@ exports.handler = async (event, context) => {
     console.log(`REQUEST: ${JSON.stringify(event)}`);
     console.log(`CONTEXT: ${JSON.stringify(context)}`);
 
+    let sendFail = (e) => {
+        console.log(`ERROR: ${e}`);
+        return response.sendFail(event, context, e.message)
+    };
+
     try {
         return (async () => require("./handler.js").doHandle(event, context))
-            .catch((e) => {
-                console.log(`REJECTED: ${e}`);
-                response.sendFail(event, context, e.message);
-            });
+            .catch((e) => sendFail(e));
     } catch (e) {
-        console.log(`ERROR: ${e}`);
-        return response.sendFail(event, context, e.message);
+        return sendFail(e);
     }
 };
