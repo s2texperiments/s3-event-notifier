c5ad4c1333ff5732d8920f1a46d8f69eeb52e73f
ste-xx <stefan.breitenstein@gmail.com>
2018-06-17 21:35:52 +0200

    fix: A conflicting conditional operation is currently in progress against this resource` when appending a S3 event

--- s3Api.js
+++ s3Api.js
@@ -3,9 +3,13 @@ const s3 = new AWS.S3();
 
 module.exports = {
     putBucketNotificationConfiguration: async (params) =>
-        new Promise((resolve, rejected) =>
-            s3.putBucketNotificationConfiguration(params, (err, data) =>
-                err ? rejected(err) : resolve(data))),
+        new Promise((resolve, rejected) => {
+            //delay timer to put Butcket Notification.
+            //Otherwise following error could occur, if multiple s3 events will be appended:
+            //A conflicting conditional operation is currently in progress against this resource`
+            setTimeout(() => s3.putBucketNotificationConfiguration(params, (err, data) => err ? rejected(err) : resolve(data)),
+                Math.floor(Math.random() * 5000));
+        }),
     getBucketNotificationConfiguration: async (params) =>
         new Promise((resolve, rejected) =>
             s3.getBucketNotificationConfiguration(params, (err, data) =>
