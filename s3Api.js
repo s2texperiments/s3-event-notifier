b6714b70c4afce4fe65d16089e1b08602b7ba9e3
ste-xx <stefan.breitenstein@gmail.com>
2018-06-17 22:01:06 +0200

    fix: A conflicting conditional operation is currently in progress against this resource` when creating a S3 event

--- s3Api.js
+++ s3Api.js
@@ -1,15 +1,32 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
+let retries = 0;
+let getDelay = () => Math.floor(Math.random() * 5000);
+
+//delay timer to put Bucket Notification.
+//Otherwise following error could occur, if multiple s3 events will be appended:
+//A conflicting conditional operation is currently in progress against this resource`
+let putBucketNotificationConfiguration = async (params) =>
+    setTimeout(() => new Promise((resolve, rejected) =>
+        s3.putBucketNotificationConfiguration(params, (err, data) =>
+            err ? rejected(err) : resolve(data))), getDelay());
+
+
+/**
+ * Retry when `A conflicting conditional operation is currently in progress against this resource` occur
+ */
+let putBucketNotificationConfigurationWithRetry = async (params) => putBucketNotificationConfiguration(params)
+    .catch(e => {
+        if (e.contains('A conflicting conditional operation is currently in progress against this resource')
+            && retries < 5) {
+            ++retries;
+            return putBucketNotificationConfigurationWithRetry(params)
+        }
+    });
+
 module.exports = {
-    putBucketNotificationConfiguration: async (params) =>
-        new Promise((resolve, rejected) => {
-            //delay timer to put Butcket Notification.
-            //Otherwise following error could occur, if multiple s3 events will be appended:
-            //A conflicting conditional operation is currently in progress against this resource`
-            setTimeout(() => s3.putBucketNotificationConfiguration(params, (err, data) => err ? rejected(err) : resolve(data)),
-                Math.floor(Math.random() * 5000));
-        }),
+    putBucketNotificationConfiguration: async (params) => putBucketNotificationConfigurationWithRetry(params),
     getBucketNotificationConfiguration: async (params) =>
         new Promise((resolve, rejected) =>
             s3.getBucketNotificationConfiguration(params, (err, data) =>
